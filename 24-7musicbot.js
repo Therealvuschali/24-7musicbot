4b9dbb6cca258ed32d2b93db805234165b199655
luuk <32298634+0300456@users.noreply.github.com>
2017-10-31 17:07:31 +0100

    Update 24-7musicbot.js

--- 24-7musicbot.js
+++ 24-7musicbot.js
@@ -22,7 +22,7 @@ client.on('message', message => {
             message.member.voiceChannel.join()
               .then(connection => { // Connection is an instance of VoiceConnection
                 message.reply('Im there m8! if i stop playing after a song just rejoin the channel');
-                connection.playStream('http://stream12.iloveradio.de/iloveradio5-aac.mp3');
+                connection.playStream('http://www.iloveradio.de/ilovemashup.m3u');
                 console.log('playing in new channel');
               })
               .catch(console.log);
