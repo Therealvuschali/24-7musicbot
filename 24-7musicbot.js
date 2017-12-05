12bc61b7df0b4ae372f52e363c31b50bbe9d6724
luuk <32298634+DELUUXE@users.noreply.github.com>
2017-12-05 23:37:25 +0100

    Update 24-7musicbot.js

--- 24-7musicbot.js
+++ 24-7musicbot.js
@@ -48,9 +48,6 @@ client.on('message', message => {
             message.reply('cannot leave a channel that im not connected to.');
         }
     }
-    if (message.content === 'musicbot crash') {
-        process.exit();
-    }
     if (message.content === 'musicbot help') {
         message.channel.send({embed: {
             color: 3447003,
